7b9ede6b55ca9b95966b04796976eb7ecb4a1383
Sourav Kumar Sheel <souravkrsheel99@gmail.com>
2025-02-03 23:56:19 +0530

    leetcode problem

--- LeetCode_Problems/Article view.sql
+++ LeetCode_Problems/Article view.sql
@@ -1,5 +1,8 @@
 ## Article view
 
+### find all the authors that viewed at least one of their own articles sorted by if in ascending order
+
+
 SELECT DISTINCT author_id as id
 FROM views
 WHERE author_id = viewer_id 
